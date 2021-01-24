3bfe63158534b743202d8d14d75bc5c08004fd78
HG05 <141k0053@itscarrillopuerto.edu.mx>
2021-01-23 18:17:30 -0600

    Septimo commit po el sacarespacios

---
+++ saludos/despedida/sacarespacios.py
@@ -0,0 +1,24 @@
+texto = input("Ingresa un texto...: ")
+unico = []
+may = []
+min = []
+for i in texto:
+    if i.isnumeric():
+        if i not in unico:
+            unico.append(i)
+            unico.sort()
+
+    elif i.isspace():
+        continue
+
+    elif isinstance(i, str):
+        if i not in may and i.isupper():
+            may.append(i)
+            may.sort()
+        elif i not in min and i.islower():
+            min.append(i)
+            min.sort()
+print(min)
+print(may)
+print(unico)
+
